4f883d28211f625f2b441fd0079e6a346cb6af62
Walter Guo <guowalter61@gmail.com>
2026-04-06 16:23:44 -0400

    14

---
+++ 2025/q14/1.py
@@ -0,0 +1,42 @@
+import copy
+with open('input.txt') as f:
+    data = f.read().splitlines()
+grid = [list(line) for line in data]
+actives = {(x, y) for x in range(len(grid)) for y in range(len(grid[0])) if grid[x][y] == "#"}
+inactives = {(x, y) for x in range(len(grid)) for y in range(len(grid[0])) if grid[x][y] == "."}
+ans = 0
+for _ in range(10):
+    new_a = set()
+    new_ia = set()
+    count = 0
+    for x, y in actives:
+        in_count = 0
+        a_count = 0
+        for nx, ny in [(x-1, y-1),(x-1, y+1),(x+1, y-1),(x+1, y+1)]:
+            if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]):
+                if(nx, ny) in actives:
+                    a_count += 1
+                else:
+                    in_count += 1
+        if a_count % 2 != 0:
+            new_a.add((x, y))
+        else:
+            new_ia.add((x, y))
+    for x, y in inactives:
+        in_count = 0
+        a_count = 0
+        for nx, ny in [(x-1, y-1),(x-1, y+1),(x+1, y-1),(x+1, y+1)]:
+            if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]):
+                if(nx, ny) in actives:
+                    a_count += 1
+                else:
+                    in_count += 1
+        if a_count % 2 != 0:
+            new_ia.add((x, y))
+        else:
+            new_a.add((x, y))
+    print(sorted(new_a))
+    ans += len(new_a)
+    actives = new_a
+    inactives = new_ia
+print(ans)
