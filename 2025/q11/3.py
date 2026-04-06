704bc64a16bae5babe6dd32c296e3cc936cb8278
Walter Guo <guowalter61@gmail.com>
2026-04-05 23:49:49 -0400

    11

--- 2025/q11/3.py
+++ 2025/q11/3.py
@@ -1,10 +1,7 @@
 with open('input.txt') as f:
     data = [int(line)for line in f.read().splitlines()]
 target = sum(data) // len(data)
-def is_balanced(nums):
-    return nums[0] == target
 check = [num for num in data if num < target] 
-print(target)
 ans = 0
 for num in check:
     ans += target - num
